c73cc02f983f023a3c8b9fd947bc23ef3b4e75a9
Netflixl <47156778+Netflixl@users.noreply.github.com>
2019-01-31 19:25:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -172,8 +172,7 @@ client.on("message", async message => {
       .setColor('#642efe')
       .setTimestamp()
      message.channel.sendEmbed(seg).then(function (message) {
-        message.react("540580008410742794")
-        message.react("540580062110679064")
+        message.react("540579817200999999")
      });
 
   }
